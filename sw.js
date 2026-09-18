9a86359c158f181aa9988890db8b288251a2c5bb
hervedibas-gif <herve.dibas@gmail.com>
2026-09-18 08:41:31 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME='mes-menus-v7-2';
+const CACHE_NAME='mes-menus-v7-3';
 const ASSETS=[
   './',
   './index.html',
-  './styles.css?v=7.1',
-  './app.js?v=7.1',
+  './styles.css?v=7.2',
+  './app.js?v=7.2',
   './manifest.webmanifest',
   './icon.svg',
   './assets/icon-120.png','./assets/icon-152.png','./assets/icon-167.png','./assets/icon-180.png','./assets/icon-192.png','./assets/icon-512.png',
